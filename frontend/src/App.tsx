f158373377fd755c31232a1e15fb7402ff3fabb5
usedigi-matheuscadenassi <matheus.cadenassi@usedigi.com.br>
2026-09-06 23:38:25 -0300

    Feat: Adiciona Page Login ao App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Heading } from "./components/Heading";
+import { Login } from "./pages/Login";
+
+export function App() {
+
+  return (
+    <main style={{width: '100vw', height: '100vh'}}>
+      <Login />
+    </main>
+  );
+}
